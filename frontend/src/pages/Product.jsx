8b15a3248d4c220db3aaa667bc458a8f9a656592
Unshack <marckfriencecoto30@gmail.com>
2024-11-25 12:21:45 +0800

    individual product data just console log

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,4 +1,28 @@
+import { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { ShopContext } from "../context/ShopContext";
+
 const Product = () => {
+  const { productId } = useParams();
+  const { products, currency } = useContext(ShopContext);
+  const [product, setProduct] = useState(false);
+  const [image, setImage] = useState("");
+  const [size, setSize] = useState("");
+
+  const fetchProductData = async () => {
+    products.map((item) => {
+      if (item._id === productId) {
+        setProduct(item);
+        setImage(item.image[0]);
+        console.log(item);
+      }
+    });
+  };
+
+  useEffect(() => {
+    fetchProductData();
+  }, [productId, products]);
+
   return <div>Product</div>;
 };
 
