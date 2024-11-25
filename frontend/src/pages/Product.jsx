73a27702bf431203a216e814324b9696195dde6b
Unshack <marckfriencecoto30@gmail.com>
2024-11-25 14:03:17 +0800

    individual product initial UI

--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -1,6 +1,11 @@
 import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
+import { FaStar, FaStarHalfStroke, FaTruckFast } from "react-icons/fa6";
+import { FaHeart } from "react-icons/fa";
+import { IoBagCheckSharp } from "react-icons/io5";
+import Footer from "../components/Footer";
+import RelatedProducts from "../components/RelatedProducts";
 
 const Product = () => {
   const { productId } = useParams();
@@ -10,20 +15,119 @@ const Product = () => {
   const [size, setSize] = useState("");
 
   const fetchProductData = async () => {
-    products.map((item) => {
-      if (item._id === productId) {
-        setProduct(item);
-        setImage(item.image[0]);
-        console.log(item);
-      }
-    });
+    const selectedProduct = products.find((item) => item._id === productId);
+    if (selectedProduct) {
+      setProduct(selectedProduct);
+      setImage(selectedProduct.image[0]);
+    }
   };
 
   useEffect(() => {
     fetchProductData();
   }, [productId, products]);
 
-  return <div>Product</div>;
+  if (!product) {
+    return <div>...Loading</div>;
+  }
+
+  return (
+    <section>
+      <div className="max-padd-container mt-8 xl:mt-6">
+        {/* product data */}
+        <div className="max-padd-container flex gap-12 flex-col xl:flex-row bg-white py-16 rounded-2xl">
+          {/* product image */}
+          <div className="flex flex-1 gap-x-2 xl:flex-1">
+            <div className="flexCenter flex-col gap-[7px] flex-wrap">
+              {product.image.map((item, i) => (
+                <img
+                  onClick={() => setImage(item)}
+                  src={item}
+                  key={i}
+                  alt="productImg"
+                  className="max-h-[89px] rounded-lg"
+                />
+              ))}
+            </div>
+            <div className="max-h-[377px] w-auto flex">
+              <img src={image} alt="" className="rounded-xl bg-gray-10" />
+            </div>
+          </div>
+          {/* product info */}
+          <div className="flex-[1.5] rounded-2xl px-7">
+            <h3 className="h3 !my-2.5">{product.name}</h3>
+            {/* rating & price */}
+            <div className="flex items-baseline gap-x-5">
+              <h3 className="h3">
+                {currency}
+                {product.price}
+              </h3>
+              <div className="flex items-center gap-x-2 text-secondary mb-2">
+                <div className="flex gap-x-2 text-secondary text-xl">
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStar />
+                  <FaStarHalfStroke />
+                </div>
+                <span>(251)</span>
+              </div>
+            </div>
+            <p>{product.description}</p>
+            <div className="flex flex-col gap-4 my-4 mb-5">
+              <div className="flex gap-2">
+                {[...product.sizes]
+                  .sort((a, b) => {
+                    const order = ["S", "M", "L", "XL", "XXL"];
+                    return order.indexOf(a) - order.indexOf(b);
+                  })
+                  .map((item, i) => (
+                    <button
+                      onClick={() => setSize(item)}
+                      key={i}
+                      className={`${
+                        item === size
+                          ? "bg-tertiary text-white"
+                          : "border-slate-900/5"
+                      } border-[1.5px] border-tertiary h-8 w-10 bg-primary rounded-md`}
+                    >
+                      {item}
+                    </button>
+                  ))}
+              </div>
+            </div>
+            <div className="flex items-center gap-x-4">
+              <button className="btn-dark w-1/2 flexCenter gap-x-2 capitalize">
+                Add to cart
+                <IoBagCheckSharp />
+              </button>
+              <button className="btn-light">
+                <FaHeart />
+              </button>
+              <div>
+                <div className="flex items-center gap-x-2 mt-2">
+                  <FaTruckFast className="text-lg" />
+                  <span className="medium-14">
+                    Free Delivery on orders over Php5000
+                  </span>
+                </div>
+                <hr className="my-4 w-2/3" />
+                <div className="mt-2 flex flex-col gap-1">
+                  <p>Authenticity You Can Trust</p>
+                  <p>Enjoy Cash On Delivery for your Convenience</p>
+                  <p>Easy Returns and Exchange Within 7 Days</p>
+                </div>
+              </div>
+            </div>
+            <RelatedProducts
+              category={product.category}
+              subCategory={product.subCategory}
+            />
+          </div>
+          <Footer />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Product;
