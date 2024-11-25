58a72fbab91a06994fa9a41e498d62bd1990fade
Unshack <marckfriencecoto30@gmail.com>
2024-11-25 14:43:43 +0800

    Related product functionality

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -4,7 +4,7 @@ import { products } from "../assets/data.js";
 export const ShopContext = createContext();
 
 const ShopContextProvider = (props) => {
-  const currency = "S";
+  const currency = "₱";
   const delivery_charges = 10;
 
   const [search, setSearch] = useState("");
