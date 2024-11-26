ee7be6d42f99cf332eb935a4b3244d054db58383
Unshack <marckfriencecoto30@gmail.com>
2024-11-26 10:50:29 +0800

    connecting to cloudinary and creating schema and routes and test it to thunderclient if its working

---
+++ backend/controllers/userController.js
@@ -0,0 +1,12 @@
+// user Login Route
+const loginUser = async (req, res) => {};
+
+// user Register Route
+const registerUser = async (req, res) => {
+  res.json({ msg: "api is working" });
+};
+
+// Admin Login Route
+const adminLogin = async (req, res) => {};
+
+export { loginUser, registerUser, adminLogin };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import "dotenv/config";
 import connectDB from "./config/mongodb.js";
+import connectCloudinary from "./config/cloudinary.js";
+import userRouter from "./routes/userRoute.js";
 
 // app config
 
@@ -9,12 +11,14 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 connectDB();
+connectCloudinary();
 
 // Middleware
 app.use(express.json());
 app.use(cors());
 
 // api endpoints
+app.use("/api/user", userRouter);
 
 app.get("/", (req, res) => {
   res.send("Api Working");
