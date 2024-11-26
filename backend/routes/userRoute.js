6c64004b1a48d04006a368f184ade398d3225819
Unshack <marckfriencecoto30@gmail.com>
2024-11-26 12:33:05 +0800

    register user and login user functionality

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -8,7 +8,7 @@ import {
 const userRouter = express.Router();
 
 userRouter.post("/register", registerUser);
-userRouter.post("/register", loginUser);
-userRouter.post("/register", adminLogin);
+userRouter.post("/login", loginUser);
+userRouter.post("/admin", adminLogin);
 
 export default userRouter;
