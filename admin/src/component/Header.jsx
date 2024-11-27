50aaeec8ec9d4b9412a9e129b0487a6f1734b3eb
Unshack <marckfriencecoto30@gmail.com>
2024-11-27 09:32:37 +0800

    Add product initial UI, consle log the result

--- admin/src/component/Header.jsx
+++ admin/src/component/Header.jsx
@@ -9,7 +9,7 @@ const Header = () => {
         className="bold-24 flex absolute left-0 right-0 w-full flexCenter"
       >
         <h4 className="shadow-sm text-secondary text-md flexCenter h-10 px-4 rounded-full">
-          Shirt Threads
+          Shirt Threads Admin
         </h4>
       </Link>
     </header>
