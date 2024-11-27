92af8c5514e5b46912a6237a4fb62a2d4e60a14d
Unshack <marckfriencecoto30@gmail.com>
2024-11-27 22:21:22 +0800

    stripe payment functionality successfully

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -1,5 +1,13 @@
 import orderModel from "../models/orderModel.js";
 import userModel from "../models/userModel.js";
+import Stripe from "stripe";
+
+// Global variables for payment
+const currency = "php";
+const deliveryCharges = 150;
+
+// stripe gateway initialization
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 
 // Place order using Cash on delivery
 const placeOrder = async (req, res) => {
@@ -28,7 +36,56 @@ const placeOrder = async (req, res) => {
 };
 
 // Place order using Stripe
-const placeOrderStripe = async (req, res) => {};
+const placeOrderStripe = async (req, res) => {
+  try {
+    const { userId, items, amount, address } = req.body;
+    const { origin } = req.headers;
+
+    const orderData = {
+      userId,
+      items,
+      amount,
+      address,
+      paymentMethod: "Stripe",
+      payment: false,
+      date: Date.now(),
+    };
+    const newOrder = new orderModel(orderData);
+    await newOrder.save();
+
+    const line_items = items.map((item) => ({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: item.name,
+        },
+        unit_amount: item.price * 100 * 277,
+      },
+      quantity: item.quantity,
+    }));
+    line_items.push({
+      price_data: {
+        currency: currency,
+        product_data: {
+          name: "Delivery charges",
+        },
+        unit_amount: deliveryCharges * 100 * 277,
+      },
+      quantity: 1,
+    });
+
+    const session = await stripe.checkout.sessions.create({
+      success_url: `${origin}/verify?success=true&orderId=${newOrder._id}`,
+      cancel_url: `${origin}/verify?success=false&orderId=${newOrder._id}`,
+      line_items,
+      mode: "payment",
+    });
+    res.json({ success: true, session_url: session.url });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // All orders data for admin panel
 const allOrders = async (req, res) => {
